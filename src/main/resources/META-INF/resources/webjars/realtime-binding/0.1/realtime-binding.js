38577464ef04bdb3e846e45d72c3be0aab1730b9
Caleb Holt <Caleb.Holt@octanner.com>
2014-07-24 23:07:44 -0600

    small fixes

--- src/main/resources/META-INF/resources/webjars/realtime-binding/0.1/realtime-binding.js
+++ src/main/resources/META-INF/resources/webjars/realtime-binding/0.1/realtime-binding.js
@@ -1,39 +1,39 @@
 function AngularSocketBinder($scope, broker, socketBase) {
 	var stompClient = null;
 	thisObject = this;
-	
+
 	var indexOf = function(data, id) {
 		for (var i = 0; i < data.length; i++)
-	        if (data[i].id === id)
-	            return i;
-	    return -1;
+			if (data[i].id === id)
+				return i;
+		return -1;
 	}
-	
-	var actionForVerb = function(verb, message, action){
-		if(message.headers.verb && message.headers.verb === verb) {
+
+	var actionForVerb = function(verb, message, action) {
+		if (message.headers.verb && message.headers.verb === verb) {
 			action(message.data, message);
 		}
 	}
-	
-	this.data = [];	
+
+	this.data = [];
 	var socket = new SockJS(broker);
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function(frame) {
-        stompClient.subscribe(socketBase, function(message){
-        	actionForVerb('create', message, function() {
-                thisObject.data.push(JSON.parse(message.body));
-        	});
-        	actionForVerb('update', message, function() {
-            	var item = JSON.parse(message.body);
-            	var index = indexOf(thisObject.data, item.id);
-                thisObject.data[index] = item;
-        	});
-        	actionForVerb('delete', message, function() {
-        		var item = JSON.parse(message.body);
-        		var index = indexOf(thisObject.data, item.id);
-            	thisObject.data.splice(index, 1);
-        	});
-           	$scope.$apply();
-        });
-    });
+	stompClient = Stomp.over(socket);
+	stompClient.connect({}, function(frame) {
+		stompClient.subscribe(socketBase, function(message) {
+			actionForVerb('create', message, function() {
+				thisObject.data.push(JSON.parse(message.body));
+			});
+			actionForVerb('update', message, function() {
+				var item = JSON.parse(message.body);
+				var index = indexOf(thisObject.data, item.id);
+				thisObject.data[index] = item;
+			});
+			actionForVerb('delete', message, function() {
+				var item = JSON.parse(message.body);
+				var index = indexOf(thisObject.data, item.id);
+				thisObject.data.splice(index, 1);
+			});
+			$scope.$apply();
+		});
+	});
 }
